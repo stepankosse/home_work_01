957745ad617d76ff2d5c60deffa347c66ee451c0
stepankosse <stepan.kosse@gmail.com>
2017-04-01 12:42:51 +0300

    Update home_work.py

--- home_work.py
+++ home_work.py
@@ -5,10 +5,13 @@ import csv
 Результат записывает в новый файл.
 
 '''
-input_file = open("need_hashes2.csv", "r")
-rdr = csv.DictReader(input_file, fieldnames=['text', 'hash', 'hashsumm'])
-output_file = open("need_hashes3.csv", "w")
-wrtr = csv.DictWriter(output_file, fieldnames=['text', 'hash', 'hashsumm'])
+
+
+input_file = open("need_hashes.csv", "r")
+rdr = csv.DictReader(input_file, delimiter=';', fieldnames=['text', 'hash', 'hashsumm'])
+output_file = open("need_hashes4.csv", "w")
+wrtr = csv.DictWriter(output_file, delimiter=';', fieldnames=['text', 'hash', 'hashsumm'])
+
 
 for line in rdr:
     hsha1 = hashlib.sha1()
@@ -17,13 +20,13 @@ for line in rdr:
     if line['hash'] == 'sha512':
         text_in_bite_code = line['text'].encode('utf-8')
         # print('text_in_bite_code: ',text_in_bite_code)
-        hsha1.update(text_in_bite_code)
+        hsha512.update(text_in_bite_code)
         # print('sha512.update(text_in_bite_code): ', hsha512.update(text_in_bite_code))
         res = hsha512.hexdigest()
         # print('Hashsumm sha512', res)
         try:
             line['hashsumm'] = res
-            print(line['hashsumm'])
+            # print(line['hashsumm'])
         except:
             pass
     elif line['hash'] == 'sha1':
@@ -41,13 +44,13 @@ for line in rdr:
     elif line['hash'] == 'md5':
         text_in_bite_code = line['text'].encode('utf-8')
         # print('text_in_bite_code: ',text_in_bite_code)
-        hsha1.update(text_in_bite_code)
+        hmd5.update(text_in_bite_code)
         # print('md5.update(text_in_bite_code): ', hmd5.update(text_in_bite_code))
         res = hmd5.hexdigest()
         # print('Hashsumm md5', res)
         try:
             line['hashsumm'] = res
-            print(line['hashsumm'])
+            # print(line['hashsumm'])
         except:
             pass
     wrtr.writerow(line)
